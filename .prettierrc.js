3992a3d4706978b6c38b4225ec881d26d6422f33
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-07-07 16:54:18 +0100

    add basic prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // enforce use of semi
+  trailingComma: "all", // print trailing comma whenever possible
+  singleQuote: true, // enforce use of single quote as they are easy to print less typing
+  printWidth: 120, // try wrap the line after 120 characters
+  tabWidth: 2,    // set number of spaces per indentation level
+};
