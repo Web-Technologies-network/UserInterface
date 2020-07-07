a3806b5465a818e21d655ad3ff67819de4a357eb
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-07-07 18:10:23 +0100

    enfore prettiers rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,9 @@ module.exports = {
   semi: true, // enforce use of semi
   trailingComma: "all", // print trailing comma whenever possible
   singleQuote: true, // enforce use of single quote as they are easy to print less typing
+  jsxSingleQuote: true, // use single quote instead of double quotes in jsx
+  bracketSpacing: true, // print spaces between brackets in object literals
   printWidth: 120, // try wrap the line after 120 characters
   tabWidth: 2,    // set number of spaces per indentation level
+  jsxBracketSameLine: false, //
 };
