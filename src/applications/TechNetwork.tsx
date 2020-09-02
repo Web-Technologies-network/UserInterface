0020a97b9706bfb915132e62bd020571a348fde5
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-09-02 19:12:59 +0100

    move dashboard to tech network application and keep index clean

--- src/applications/TechNetwork.tsx
+++ src/applications/TechNetwork.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './TechNetwork.css';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+// import './TechNetwork.css';
 import { Dashboard } from '../components/Dashboard';
+import '../assets/css/material-dashboard-react.css?v=1.9.0';
 
+const hist = createBrowserHistory();
 const TechNetwork: React.FC = () => (
-  <div className='App'>
-    <Dashboard />
-  </div>
+  <Router history={hist}>
+    <Switch>
+      <Route path='/' component={Dashboard} />
+    </Switch>
+  </Router>
 );
 
 export default TechNetwork;
--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TechNetwork from './applications/TechNetwork';
-import { createBrowserHistory } from 'history';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
-import './assets/css/material-dashboard-react.css?v=1.9.0';
-import { Dashboard } from './components/Dashboard';
 
-const hist = createBrowserHistory();
-
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path='/' component={Dashboard} />
-    </Switch>
-  </Router>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<TechNetwork />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
