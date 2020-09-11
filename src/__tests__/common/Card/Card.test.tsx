ad1bcf316cc9b4660e411c0a9e23c204a0c83e2c
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-09-12 00:29:22 +0100

    test danger and card components

--- src/__tests__/common/Card/Card.test.tsx
+++ src/__tests__/common/Card/Card.test.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Card } from '../../../components/common';
 
-describe('Card Rendering:', () => {
+/**
+ * Test suite
+ */
+describe('Card:', () => {
+  /**
+   * Test case
+   */
   test('should render the same', () => {
+    //prepare
+    const children = <div>Test children</div>;
     //act
     const tree = renderer.create(
       <Card chart={<div>Chart</div>} className={null} plain={null} profile={null}>
-        <div>Test children</div>
+        {children}
       </Card>,
     );
+    //assert
     expect(tree.toJSON()).toMatchInlineSnapshot(`
       <div
         className="MuiGrid-root makeStyles-card-1 makeStyles-cardChart-4 null"
