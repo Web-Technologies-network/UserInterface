44598233caabc380723323f9ef5394cbd0f0797d
david.mccabe <david.mccabe082@gmail.com>
2020-09-14 22:32:04 +0100

    minor correction to Info component

--- src/components/common/Typography/info.tsx
+++ src/components/common/Typography/info.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { typographyClasses as classes } from '../../../assets/jss/material-dashboard-react/components/typographyStyle';
 import { Grid } from '@material-ui/core';
 
-
 export const Info: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <Grid className={classes.defaultFontStyle + ' ' + classes.dangerText}>{children}</Grid>
+  <Grid className={classes.defaultFontStyle + ' ' + classes.infoText}>{children}</Grid>
 );
