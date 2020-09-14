ea753293efa304f723b57d0b9b78e9d85df97c0f
david.mccabe <david.mccabe082@gmail.com>
2020-09-14 23:20:37 +0100

    further minor corrections to Info component

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,5 +1,5 @@
 export { Card } from './Card/Card';
 export { Danger } from './Typography/Danger';
-export { Info } from './Typography/info';
+export { Info } from './Typography/Info';
 export { Success } from './Typography/Success';
 export { Warning } from './Typography/Warning';
