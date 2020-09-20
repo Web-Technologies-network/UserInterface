b0e8e0629d97a91cbaef520ef7be0ce5a9ba080e
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-09-20 14:52:19 +0100

    fix merging issues

--- src/components/common/Typography/Info.tsx
+++ src/components/common/Typography/Info.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { typographyClasses as classes } from '../../../assets/jss/material-dashboard-react/components/typographyStyle';
+import { createTypographyStyle } from '../../../assets/jss/material-dashboard-react/components/typographyStyle';
 import { Grid } from '@material-ui/core';
 
-export const Info: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+export const Info: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+ const classes = createTypographyStyle();
+ return(
   <Grid className={classes.defaultFontStyle + ' ' + classes.infoText}>{children}</Grid>
-);
+ );
+}
--- src/components/common/Typography/Success.tsx
+++ src/components/common/Typography/Success.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { typographyClasses as classes } from '../../../assets/jss/material-dashboard-react/components/typographyStyle';
+import { createTypographyStyle } from '../../../assets/jss/material-dashboard-react/components/typographyStyle';
 import { Grid } from '@material-ui/core';
 
-export const Success: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+export const Success: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+ const classes = createTypographyStyle();
+ return (
   <Grid className={classes.defaultFontStyle + ' ' + classes.successText}>{children}</Grid>
-);
+ );
+}
