f8e31149b7264d603447c6fe840a216626708d80
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-10-19 20:40:15 +0100

    create the app store with dev tools and loger middleware in dev env whiel with empty middleware for now in production

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,4 +1,16 @@
-import { combineReducers } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { logger } from 'redux-logger';
 import { profiles } from './profile';
+import { composeWithDevTools } from 'redux-devtools-extension';
 const appReducer = combineReducers({ profiles });
 export type RootState = ReturnType<typeof appReducer>;
+
+const isDev = (): boolean => window.location.toString().includes('localhost');
+
+const middlewares = [];
+
+if (isDev()) middlewares.push(logger);
+
+export const store = isDev()
+  ? createStore(appReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+  : createStore(appReducer, applyMiddleware(...middlewares));
