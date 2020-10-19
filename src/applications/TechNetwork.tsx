5b672977ce0f063b84e310a22af94d3fe1b545bb
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-10-19 20:41:45 +0100

    make redux store available to connect calls for the all app components

--- src/applications/TechNetwork.tsx
+++ src/applications/TechNetwork.tsx
@@ -4,14 +4,18 @@ import { Router, Route, Switch } from 'react-router-dom';
 // import './TechNetwork.css';
 import { Dashboard } from '../components/Dashboard';
 import '../assets/css/material-dashboard-react.css?v=1.9.0';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 
 const hist = createBrowserHistory();
 const TechNetwork: React.FC = () => (
-  <Router history={hist}>
-    <Switch>
-      <Route path='/' component={Dashboard} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router history={hist}>
+      <Switch>
+        <Route path='/' component={Dashboard} />
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default TechNetwork;
