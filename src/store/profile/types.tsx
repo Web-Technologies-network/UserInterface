aa4cba741a2b6d106f530dc39ea3c141f46a5764
Alhaytham Elhassan <asalhaytham@gmail.com>
2020-10-19 18:39:22 +0100

    define typescript types for action models, define actionstypes and export a union of actions typescript types

---
+++ src/store/profile/types.tsx
@@ -0,0 +1,28 @@
+// Model typescript types
+export interface Profile {
+  id: string;
+  name: string;
+}
+
+// action types
+export const ADD_PROFILE = 'ADD_PROFILE';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const DELETE_PROFILE = 'DELETE_PROFILE';
+
+// action typescript types
+interface AddProfileAction {
+  type: typeof ADD_PROFILE;
+  payload: Profile;
+}
+
+interface UpdateProfile {
+  type: typeof UPDATE_PROFILE;
+  payload: Profile;
+}
+
+interface DeleteProfile {
+  type: typeof DELETE_PROFILE;
+  payload: Profile;
+}
+// export a union of actions typescript types
+export type ProfileActionTypes = AddProfileAction | UpdateProfile | DeleteProfile;
